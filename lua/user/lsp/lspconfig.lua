2145a3486fc5c0267b8504e95c1da593c93ebc42
mkinet <maxime.kinet@nexthink.com>
2022-11-22 10:08:53 +0100

    lua file formatting

--- lua/user/lsp/lspconfig.lua
+++ lua/user/lsp/lspconfig.lua
@@ -1,25 +1,25 @@
 on_attach = require("user.lsp.handlers").on_attach
 
 require("lspconfig")["pyright"].setup({
-	on_attach = on_attach,
+  on_attach = on_attach,
 })
 require("lspconfig")["jsonls"].setup({
-	on_attach = on_attach,
+  on_attach = on_attach,
 })
 require("lspconfig")["sumneko_lua"].setup({
-	on_attach = on_attach,
-	-- Server-specific settings...
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
+  on_attach = on_attach,
+  -- Server-specific settings...
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim" },
+      },
+      workspace = {
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+    },
+  },
 })
