5cb775dcc9f9722d61e34608bfcdca86ebd8341b
Maxime Kinet <maxime.kinet@nexthink.com>
2025-09-01 22:38:22 +0200

    update venvselect, telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,87 +1,105 @@
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.6",
-    dependencies = {
-        { "nvim-lua/plenary.nvim" },
-        {
-            "isak102/telescope-git-file-history.nvim",
-            dependencies = { "tpope/vim-fugitive" }
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    {
+      "isak102/telescope-git-file-history.nvim",
+      dependencies = { "tpope/vim-fugitive" }
+    }
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
+    local utils = require("telescope.utils")
+    local gfh_actions = require("telescope").extensions.git_file_history.actions
+     -- robust project root helper: prefer git toplevel, else buffer dir
+    local function get_wd()
+      local dir = utils.buffer_dir()
+      -- use `-C` and quote the path
+      local inside = vim.fn.system({ "bash", "-lc",
+        "git -C " .. vim.fn.shellescape(dir) .. " rev-parse --is-inside-work-tree 2>/dev/null || echo false"
+      }):gsub("%s+$","")
+      if inside == "true" then
+        local top = vim.fn.system({ "bash", "-lc",
+          "git -C " .. vim.fn.shellescape(dir) .. " rev-parse --show-toplevel"
+        })
+        return top:gsub("%s+$","")
+      end
+      return dir
+    end
+    -- Lua-pattern, case-insensitive where needed:
+    --  - escape dots with %.
+    --  - use [Pp] to match both cases.
+    local IGNORE = {
+      "[Pp]ictures",
+      "[Dd]esktop",
+      "[Ll]ibrary",
+      "__pycache__",
+      "%%.git",               -- literal ".git"
+      "%%.ipynb_checkpoints", -- literal ".ipynb_checkpoints"
+      ".venv",              -- literal ".venv"
+    }
+    telescope.setup {
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        path_display = { "smart" },
+      },
+      pickers = {
+        -- Default configuration for builtin pickers goes here:
+        find_files = {
+          cwd = get_wd(),
+          file_ignore_patterns = IGNORE,  -- Use the IGNORE list to exclude specific patterns
+          -- Use fd command to include hidden files and .env files while excluding specific directories
+          find_command = { 
+            "fd", 
+            "--type", "f",
+            "--hidden",
+            "--follow",
+            "--exclude", ".venv",
+            "--exclude", ".git", 
+            "--exclude", "__pycache__",
+            "--exclude", ".ipynb_checkpoints"
+          }
+        },
+        live_grep = {
+          cwd = get_wd(),
+          additional_args = function(opts)
+            -- Include hidden files but respect .gitignore
+            return { "--hidden", "--glob", "!.git", "--glob", "!.venv", "--glob", "!__pycache__", "--glob", "!.ipynb_checkpoints" }
+          end,
+          file_ignore_patterns = IGNORE,  -- Use the IGNORE list to exclude specific patterns
         }
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
-        local builtin = require("telescope.builtin")
-        local utils = require("telescope.utils")
-        local gfh_actions = require("telescope").extensions.git_file_history.actions
-        local function get_wd()
-            -- get start directory for pickers
-            -- logic is as follows :
-            --  - always use the buffer directory and no the directory where vim was startedas
-            --    as starting point
-            --  - if the current buffer is within a git project, search the whole project
-            --  - else use current buffer directory as root
-            -- Note ; the -C arg to the git command is used to run the git command in a
-            --      directory that is not the current working directory (remember the
-            --      current buffer is not necessarily in the current working directory)
-            local cmd = "git -C " .. utils.buffer_dir() .. " rev-parse --is-inside-work-tree"
-            local is_git_0 = vim.fn.system(cmd)
-            if string.match(is_git_0, 'true') then
-                local cmd = "git -C " .. utils.buffer_dir() .. " rev-parse --show-toplevel"
-                return vim.fn.systemlist(cmd)[1]
-            else
-                return utils.buffer_dir()
-            end
-        end
-        telescope.setup {
-            defaults = {
-                prompt_prefix = " ",
-                selection_caret = " ",
-                path_display = { "smart" },
-                file_ignore_patterns = { "Pictures", "Desktop", "Library", "__pycache__", ".git", ".ipynb_checkpoints" },
+        -- Now the picker_config_key will be applied every time you call this
+        -- builtin picker
+      },
+      extensions = {
+        git_file_history = {
+          -- Keymaps inside the picker
+          mappings = {
+            i = {
+              ["<C-g>"] = gfh_actions.open_in_browser,
             },
-            pickers = {
-                -- Default configuration for builtin pickers goes here:
-                find_files = {
-                    cwd = get_wd(),
-                    no_ignore = true,
-                    hidden = true,
-                },
-                live_grep = {
-                    cwd = get_wd(),
-                    additional_args = function(opts)
-                        return {"--no-ignore", "--hidden"}
-                    end,
-                }
-                -- Now the picker_config_key will be applied every time you call this
-                -- builtin picker
+            n = {
+              ["<C-g>"] = gfh_actions.open_in_browser,
             },
-            extensions = {
-                git_file_history = {
-                    -- Keymaps inside the picker
-                    mappings = {
-                        i = {
-                            ["<C-g>"] = gfh_actions.open_in_browser,
-                        },
-                        n = {
-                            ["<C-g>"] = gfh_actions.open_in_browser,
-                        },
-                    },
-                    -- The command to use for opening the browser (nil or string)
-                    -- If nil, it will check if xdg-open, open, start, wslview are available, in that order.
-                    browser_command = nil,
-                },
-            },
-        }
-        -- set keymaps
-        local keymap = vim.keymap
+          },
+          -- The command to use for opening the browser (nil or string)
+          -- If nil, it will check if xdg-open, open, start, wslview are available, in that order.
+          browser_command = nil,
+        },
+      },
+    }
+    -- set keymaps
+    local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
-        keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
-        keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
-        keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Find string under cursor in cwd" })
-        keymap.set("n", "<leader>fc", "<cmd>Telescope git commits<cr>", { desc = "Find todos" })
-        keymap.set("n", "<leader>fh", "<cmd>Telescope git_file_history<cr>", { desc = "Search Git history" })
-    end,
-    keys = { "<leader>ff", "<leader>fg", "<leader>fb", "<leader>fs", "<leader>fc", "<leader>fh" }
+    keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope git_status<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope git commits<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope git_file_history<cr>", { desc = "Search Git history" })
+  end,
+  keys = { "<leader>ff", "<leader>fg", "<leader>fb", "<leader>fs", "<leader>fc", "<leader>fh" }
 }
--- lua/plugins/venvselect.lua
+++ lua/plugins/venvselect.lua
@@ -1,28 +1,25 @@
 return {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "neovim/nvim-lspconfig",
-        "mfussenegger/nvim-dap",
-        "mfussenegger/nvim-dap-python", --optional
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "neovim/nvim-lspconfig",
+    -- "mfussenegger/nvim-dap",
+    -- "mfussenegger/nvim-dap-python", --optional
+  },
+  ft = "python",
+  opts = {
+    search = {
+      -- my_venvs = {
+      --   -- Quote the path and use regex that specifically looks for .venv directories
+      --   command = "fd 'bin/python$' $CWD --full-path --color never -HI -a -L",
+      -- },
     },
-    lazy = true,
-    branch = "regexp", -- This is the regexp branch, use this for the new version
-    opts = {
-        settings = {
-            search = {
-                cwd = false,                               -- setting this to false disables the default cwd search
-                workspace = false,
-                file = false,
-                pipx = false,
-                virtualenvs=false,
-                poetry = {
-                    command = "fd --full-path -HI /bin/python$ $(git rev-parse --show-toplevel)"
-                }
-            },
-        },
-    },
-    keys = {
-        { "<leader>v", "<cmd>VenvSelect<cr>" },
-    },
+    options = {
+      debug = true,  -- Enable debug logging
+      notify_user_on_activate = true,
+    }
+  },
+  keys = {
+    { "<leader>v", "<cmd>VenvSelect<cr>" },
+  },
 }
